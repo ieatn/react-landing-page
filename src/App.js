d62c2a724fac22f0475d1c0e83f1caeff42c9601
ieatn <ieatnubcake@gmail.com>
2022-11-30 15:05:00 -0800

    navbar outline done

--- src/App.js
+++ src/App.js
@@ -7,15 +7,6 @@ import './App.css';
 const App = () => (
   <div>
     <Navbar />
-    <Header />
-    <AboutUs />
-    <SpecialMenu />
-    <Chef />
-    <Intro />
-    <Laurels />
-    <Gallery />
-    <FindUs />
-    <Footer />
   </div>
 );
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,7 +4,24 @@ import './Navbar.css';
 
 const Navbar = () => (
   <div>
-    Navbar
+    <nav className='navbar'>
+      <div className="left">
+        Gericht
+      </div>
+      <div className="middle">
+        <ul>
+          <li><a href="">Home</a></li>
+          <li><a href="">Home</a></li>
+          <li><a href="">Home</a></li>
+          <li><a href="">Home</a></li>
+          <li><a href="">Home</a></li>
+        </ul>
+      </div>
+      <div className="right">
+        <a href="">Log In/Register</a>
+        <a href="">Book Table</a>
+      </div>
+    </nav>
   </div>
 );
 
