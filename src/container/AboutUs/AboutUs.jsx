dfd46c97a47bf5b1bb2b016ca0aa3b84c8c21d2d
ieatn <ieatnubcake@gmail.com>
2022-12-02 14:38:39 -0800

    about us section, media queries for header

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,21 +1,27 @@
 import React from 'react';
 
 import './AboutUs.css';
+import {images} from '../../constants'
 
 const AboutUs = () => (
-  <div className='about-us'>
+  <div className='about-us app__bg'>
+    <div className="about-us-bg flex-center">
+      <img src={images.G} alt="" />
+    </div>
     <div className="about-left">
-      <h1>About Us</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores temporibus quod ipsam sit. Laborum, perspiciatis?</p>
-      <button className='about-left-btn'>Read More</button>
+      <h1 className='headtext__cormorant'>About Us</h1>
+      <img src={images.spoon} alt="" />
+      <p className='p__opensans'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores temporibus quod ipsam sit. Laborum, perspiciatis?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore, architecto?</p>
+      <button className='about-left-btn custom__button'>Read More</button>
     </div>
     <div className="about-middle">
-      Divider
+      <img src={images.knife} alt="" />
     </div>
     <div className="about-right">
-      <h1>Our History</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores temporibus quod ipsam sit. Laborum, perspiciatis?</p>
-      <button>Read More</button>
+      <h1 className='headtext__cormorant'>Our History</h1>
+      <img src={images.spoon} alt="" />
+      <p className='p__opensans'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores temporibus quod ipsam sit. Laborum, perspiciatis?Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem, nulla?</p>
+      <button className='custom__button'>Know More</button>
     </div>
   </div>
 );
