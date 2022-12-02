401fe019013e138844a87be2d7227a4f51dd3d6b
ieatn <ieatnubcake@gmail.com>
2022-12-02 15:12:46 -0800

    work in progress on menu section

--- src/components/Menuitem/MenuItem.jsx
+++ src/components/Menuitem/MenuItem.jsx
@@ -2,9 +2,19 @@ import React from 'react';
 
 import './MenuItem.css';
 
-const MenuItem = () => (
+const MenuItem = ({title, price}) => (
   <div>
-    MenuItem
+    <div>
+      <div>
+        <p className='p__cormorant' style={{color: '#DDCA87'}}>{title}</p>
+      </div>
+    </div>
+    
+    <div>
+      <div>
+        <p className='p__cormorant'>{price}</p>
+      </div>
+    </div>
   </div>
 );
 
--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -1,28 +1,39 @@
 import React from 'react';
 
 import './SpecialMenu.css';
-// have to import beer and cocktail data into usestate, map over them and display
+import {images, data} from '../../constants'
+import {SubHeading, MenuItem} from '../../components'
 
 const SpecialMenu = () => (
-  <div className='menu'>
+  <div className='menu' id='menu'>
     <div className="menu-header">
-      <p>Menu That Fits Your Palatte</p>
-      <h1>Today's Special</h1>
+      <SubHeading title="Menu That Fits Your Palatte"/>
+      <h1 className='headtext__cormorant'>Today's Special</h1>
     </div>
     <div className="menu-container">
       <div className="menu-left">
-        <h2>Wine & Beer</h2>
-        {/* <p>map over beer data</p> */}
+        <h2 className='wine-heading'>Wine & Beer</h2>
+        <div className="wine-items">
+          {data.wines.map((wine, index) => (
+            <MenuItem key={wine.title + index} title={wine.title} price={wine.price} />
+          ))}
+        </div>
       </div>
       <div className="menu-middle">
-        cocktail image
+        <img src={images.menu} alt="" />
       </div>
       <div className="menu-right">
-        <h2>Cocktails</h2>    
-        {/* <p>map over cocktail data</p> */}
+        <h2 className='cocktails-heading'>Cocktails</h2>
+        <div className="cocktails-items">
+          {data.cocktails.map((cocktail, index) => (
+            <MenuItem key={cocktail.title + index} title={cocktail.title} price={cocktail.price} />
+          ))}
+        </div>
+      </div>
+      <div style={{ marginTop: 15 }}>
+        <button className="custom__button">View More</button>
       </div>
     </div>
-    <button className="menu-btn">View More</button>
   </div>
 );
 
