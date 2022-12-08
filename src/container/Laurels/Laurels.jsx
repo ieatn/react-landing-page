e5231d5245f1e43dfb7758f63f21666c4705d018
ieatn <ieatnubcake@gmail.com>
2022-12-07 18:02:20 -0800

    finished laurels

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -1,37 +1,32 @@
 import React from 'react';
 
 import './Laurels.css';
+// import data array which contain awards url, title, subtitle
+import {images, data} from '../../constants'
+import {SubHeading} from '../../components'
+
+// create a component for each award item, destructure the property names from each object
+const AwardCard = ({award: {imgUrl, title, subtitle}}) => (
+  <div className="award-card">
+    <img src={imgUrl} alt="award" />
+    <p className="p__cormorant">{title}</p>
+    <p className="p__cormorant">{subtitle}</p>
+  </div>
+)
 
 const Laurels = () => (
-  <div className='laurels'>
+  // bg for rocky background image, wrapper for display flex, padding for everything
+  <div className='laurels app__bg app__wrapper section__padding'>
     <div className="laurels-left">
-      <p>Awards & Recognition</p>
-      <h1 className='award-title'>Our Laurels</h1>
-      <ul>
-        <li>
-          {/* img logo of award */}
-          <p className='award-title'>Bib Gourmond</p>
-          <p className='award-text'>Lorem ipsum dolor sit amet consectetur.</p>
-        </li>
-        <li>
-          {/* img logo of award */}
-          <p className='award-title'>Bib Gourmond</p>
-          <p className='award-text'>Lorem ipsum dolor sit amet consectetur.</p>
-        </li>
-        <li>
-          {/* img logo of award */}
-          <p className='award-title'>Bib Gourmond</p>
-          <p className='award-text'>Lorem ipsum dolor sit amet consectetur.</p>
-        </li>
-        <li>
-          {/* img logo of award */}
-          <p className='award-title'>Bib Gourmond</p>
-          <p className='award-text'>Lorem ipsum dolor sit amet consectetur.</p>
-        </li>
-      </ul>
+      <SubHeading title={'Awards & Recognition'}/>
+      <h1 className='headtext__cormorant'>Our Laurels</h1>
+      {/* map over each item in awards array in data file and print them out */}
+      <div className="awards">
+        {data.awards.map((i) => <AwardCard award={i} key={i.title}/>)}
+      </div>
     </div>
     <div className="laurels-right">
-      <img src="https://i.kym-cdn.com/entries/icons/original/000/029/857/Shirogane_learns_Volleyball_2-3_screenshot.png" alt="" />
+      <img className='laurel-img' src={images.laurels} alt="" />
     </div>
   </div>
 );
